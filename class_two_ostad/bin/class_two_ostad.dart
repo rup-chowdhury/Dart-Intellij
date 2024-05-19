fc029d426f017dc02ba096c462f676a56f2bbd47
Rup Chowdhury <rupbd1@gmail.com>
2024-05-19 22:32:28 +0600

    New project for module 2

---
+++ class_two_ostad/bin/class_two_ostad.dart
@@ -0,0 +1,5 @@
+import 'package:class_two_ostad/class_two_ostad.dart' as class_two_ostad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${class_two_ostad.calculate()}!');
+}
---
+++ class_two_ostad/test/class_two_ostad_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_two_ostad/class_two_ostad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
