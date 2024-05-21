79ef690f2f3e1dbf720acf40af5f5c9da0d29485
Rup Chowdhury <rupbd1@gmail.com>
2024-05-21 22:24:44 +0600

    create new project

---
+++ class_three_ostad/bin/class_three_ostad.dart
@@ -0,0 +1,5 @@
+import 'package:class_three_ostad/class_three_ostad.dart' as class_three_ostad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${class_three_ostad.calculate()}!');
+}
---
+++ class_three_ostad/test/class_three_ostad_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_three_ostad/class_three_ostad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
