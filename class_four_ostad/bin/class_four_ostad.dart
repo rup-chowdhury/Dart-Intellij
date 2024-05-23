83e7802f6d0cb70f9d5fa4cc1d9ce00eeba37500
Rup Chowdhury <rupbd1@gmail.com>
2024-05-23 22:34:54 +0600

    Create project

---
+++ class_four_ostad/bin/class_four_ostad.dart
@@ -0,0 +1,8 @@
+main() {
+  String name = 'Rup';
+
+  var cgpa = 3.91;
+  print(cgpa);
+  print(cgpa.runtimeType);
+  
+}
---
+++ class_four_ostad/test/class_four_ostad_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_four_ostad/class_four_ostad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
