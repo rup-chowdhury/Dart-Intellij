e808a4f45e815440c6f290e95d42e6d10d9d00fb
Rup Chowdhury <rupbd1@gmail.com>
2024-05-26 23:01:49 +0600

    Update func.dart

--- func/bin/func.dart
+++ func/bin/func.dart
@@ -1,13 +1,23 @@
 main(){
 
-  String name;
+  String name, address, age;
 
   name = 'Rup Chowdhury';
 
-  welcomeMessage(name);
+  address = '33/2 Commisioner Goli';
+
+  age = '25';
+
+  welcomeMessage(name, address, age);
+
+  name = 'Hackerman';
+
+  address = 'hidden';
+
+  welcomeMessage(name, address);
 
 }
 
-welcomeMessage(String name){
-  print('Welcome to Flutter programming Mr. $name');
+welcomeMessage(String name, String address, [String age = 'Confidential']){
+  print('Welcome to Flutter programming Mr. $name livin in $address and age of $age' );
 }
