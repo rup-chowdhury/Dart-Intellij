a7f3af4e8d683f3c29cca470d84b397d56cd40dd
Rup Chowdhury <rupbd1@gmail.com>
2024-05-28 22:58:03 +0600

    Update for_loop.dart

--- func/bin/for_loop.dart
+++ func/bin/for_loop.dart
@@ -1,23 +1,33 @@
 void main(){
 
-  for (int loop = 0 ; loop <= 10 ; loop = loop+1){
-    if(loop == 5){
-    continue;
-    }
+  // for (int loop = 0 ; loop <= 10 ; loop = loop+1){
+  //   if(loop == 5){
+  //   continue;
+  //   }
+  //
+  //   if(loop == 8){
+  //     break;
+  //   }
+  //   print(loop);
+  //   greeting();
+  // }
 
-    if(loop == 8){
-      break;
-    }
-    print(loop);
-    greeting();
-  }
 
-
-  int j = 1;
-
-  while(j <= 5){
-    print(j);
-    j++;
+  // int j = 1;
+  //
+  // while(j <= 5){
+  //   print(j);
+  //   j++;
+  // }
+  
+  List<String> students = [
+    'Rup',
+    'Ratan',
+    'Laal'
+  ];
+  
+  for (int i = 0; i <= students.length; i++) {
+    print('Student $i  : ')
   }
 
 }
