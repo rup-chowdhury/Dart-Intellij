daf78c3e56655931cba21085224adc1608ca7793
Rup Chowdhury <rupbd1@gmail.com>
2024-05-30 23:12:19 +0600

    Update exception_handling.dart

--- func/bin/exception_handling.dart
+++ func/bin/exception_handling.dart
@@ -6,6 +6,8 @@ main(){
     String input = '34          i';
     int parsedValue = int.parse(input);
     print(parsedValue);
+  }on FormatException {
+    print('This is format exception');
   }
   catch(e) {
     print(e.toString());
