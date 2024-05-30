595e86fdb7910637784e9fd7ab55af95d662c07a
Rup Chowdhury <rupbd1@gmail.com>
2024-05-30 23:39:21 +0600

    Update exception_handling.dart

--- func/bin/exception_handling.dart
+++ func/bin/exception_handling.dart
@@ -8,8 +8,12 @@ main(){
     String input = '34          i';
     int parsedValue = int.parse(input);
     print(parsedValue);
-  }on Socket{
+  }
+  on Socket{
     print('This is Socket exception');
+  }
+  on myExeption{
+    print('Custom Exeption found');
   }
   on FormatException {
     print('This is format exception');
@@ -17,7 +21,8 @@ main(){
   catch(e) {
     print(e.toString());
     print('You have faced a runtime error');
-  } finally {                                            // This must be executed
+  }
+  finally {                                            // This must be executed
     print('Finally, this will be printed');
   }
 
