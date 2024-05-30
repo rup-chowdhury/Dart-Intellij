399d68e98330a3addeca647224d7aed32e406c47
Rup Chowdhury <rupbd1@gmail.com>
2024-05-30 22:51:13 +0600

    Update null_safety.dart

--- func/bin/null_safety.dart
+++ func/bin/null_safety.dart
@@ -1,14 +1,22 @@
 void main(){
-
-
   // ?, ??, !..........these are used to make a variable nullable
 
   int? myAge = null;          // This is nullable integer
-
   // myAge = 25;
+  print(myAge.runtimeType);               //Null
 
-  print(myAge.runtimeType);
+
+  String? name = null;       // This is nullable String
+  print(name.runtimeType);        // Null
+  name = ('Rup');
+  print(name.runtimeType);       // String
 
 
-  
+
+  String? income;
+
+  print(income ?? 'Null value');
+
+  print(income);
+
 }
