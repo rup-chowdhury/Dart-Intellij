4abc6fe2e5b4f67532608e2987f64184a1abea3f
Rup Chowdhury <rupbd1@gmail.com>
2024-05-30 23:11:16 +0600

    Update exception_handling.dart

--- func/bin/exception_handling.dart
+++ func/bin/exception_handling.dart
@@ -1,3 +1,16 @@
 main(){
 
+  //try catch
+
+  try {
+    String input = '34          i';
+    int parsedValue = int.parse(input);
+    print(parsedValue);
+  }
+  catch(e) {
+    print(e.toString());
+    print('You have faced a runtime error');
+  }
+
+  print('Hello World');
 }
