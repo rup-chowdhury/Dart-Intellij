e55c8eba3f8ad6af5bdbf30b9fcd38b58957dd98
Rup Chowdhury <rupbd1@gmail.com>
2024-06-07 22:31:35 +0600

    Update human.dart

---
+++ dart_oop/bin/human.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name = 'Rup';
+  int age = 25;
+  String address = 'Kamalapur';
+
+
+  String name2 = 'Ruplaal';
+  int age2 = 45;
+  String address2 = 'Kamalapur';
+}
+
+class Human {
+
+}
