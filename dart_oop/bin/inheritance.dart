169e26b1b222073c937b54f670ac05202ae81b79
Rup Chowdhury <rupbd1@gmail.com>
2024-06-09 21:54:31 +0600

    Update inheritance.dart

--- dart_oop/bin/inheritance.dart
+++ dart_oop/bin/inheritance.dart
@@ -12,16 +12,6 @@ class Parrot extends Bird{
   }
 }
 
-// Multi level inheritance\
-
-class Eagle extends Bird{
-  void vision(){
-    print('Eagle has a sharp vision');
-  }
-}
-
-// Note - Dart doesn't support multiple inheritance because it creates complexity in the program.
-
 main(){
   print('Inheritance');
 
@@ -30,9 +20,5 @@ main(){
   p.fly();
 
   p.speak();
-
-  Eagle e = Eagle();
-
-  e.vision();
 
 }
