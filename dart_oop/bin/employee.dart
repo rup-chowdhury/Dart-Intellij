556dd33fc9479d09e07abeda833ceffaf474a548
Rup Chowdhury <rupbd1@gmail.com>
2024-06-13 22:37:51 +0600

    Update employee.dart

---
+++ dart_oop/bin/employee.dart
@@ -0,0 +1,17 @@
+// contract
+
+abstract class Employee {
+  void work();
+
+  void formalDressUp();
+
+  void attendMeeting();
+
+  void goToOffice() {
+    print('Print Something');
+  }
+}
+
+class SoftwareEngineer {
+  void att
+}
