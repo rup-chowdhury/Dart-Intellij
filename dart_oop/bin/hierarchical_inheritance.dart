e71eac652e6684305bcd94fb8d434d8fcd9ba39a
Rup Chowdhury <rupbd1@gmail.com>
2024-06-14 23:39:58 +0600

    Update hierarchical_inheritance.dart

--- dart_oop/bin/hierarchical_inheritance.dart
+++ dart_oop/bin/hierarchical_inheritance.dart
@@ -4,5 +4,16 @@ class Person {
     print(name);
   }
 
-  
+  void dispAge(int age){
+    print(age);
+  }
 }
+
+class Peter extends Person {
+
+  void dispBrach(String nationality){
+    print(nationality);
+  }
+}
+
+//Derived class created from another derived class 
