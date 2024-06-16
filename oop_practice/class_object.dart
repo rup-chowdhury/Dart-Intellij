a27c54535cfef7506d33d6b013e4f5c369d049bc
Rup Chowdhury <rupbd1@gmail.com>
2024-06-17 00:12:22 +0600

    method

--- oop_practice/class_object.dart
+++ oop_practice/class_object.dart
@@ -4,6 +4,12 @@ class Monitor{
   // Attribute / properties
   String companyName = 'LG';
   String model = 'K300';
+  String screenSize = '17';
+
+  //method
+  void printScreenSize(){
+    print(screenSize);
+  }
 }
 
 void main(){
