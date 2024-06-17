263cc78b4e4ea544b5d678dfc04e61d36083d907
Rup Chowdhury <rupbd1@gmail.com>
2024-06-17 23:46:35 +0600

    commenting out unnecessary code

--- oop_practice/main.dart
+++ oop_practice/main.dart
@@ -1,27 +1,27 @@
 import 'class_object.dart';
 
 void main(){
-  Monitor myMonitor = Monitor();
-
-  print(myMonitor.companyName);
-  print(myMonitor.model);
+  Monitor myMonitor = Monitor( 'LG', 'K 150');
+  //
+  // print(myMonitor.companyName);
+  // print(myMonitor.model);
   myMonitor.printScreenSize();
 
-  Monitor anikMonitor = Monitor();
+  Monitor anikMonitor = Monitor('Samsung', 'A7');
 
-  anikMonitor.companyName = 'Samsung';
-  anikMonitor.model = 'A7';
+  // anikMonitor.companyName = 'Samsung';
+  // anikMonitor.model = 'A7';
 
 
-  print(anikMonitor.companyName);
-  print(anikMonitor.model);
+  // print(anikMonitor.companyName);
+  // print(anikMonitor.model);
 
-  Monitor rupMonitor = Monitor();
+  Monitor rupMonitor = Monitor('Chinese', 'Unknown');
 
-  rupMonitor.companyName = 'Chinese';
-  rupMonitor.model = 'Unknown';
-
-  print(rupMonitor.companyName);
-  print(rupMonitor.model);
+  // rupMonitor.companyName = 'Chinese';
+  // rupMonitor.model = 'Unknown';
+  //
+  // print(rupMonitor.companyName);
+  // print(rupMonitor.model);
   // print(rupMonitor._internalHardwareNumber);
 }
