9ae8c491857584d60eb9336912a7c1593ddad3a0
Rup Chowdhury <rupbd1@gmail.com>
2024-06-17 10:09:18 +0600

    remove main class from here

--- oop_practice/class_object.dart
+++ oop_practice/class_object.dart
@@ -12,29 +12,3 @@ class Monitor{
     print(screenSize);
   }
 }
-
-void main(){
-  Monitor myMonitor = Monitor();
-
-  print(myMonitor.companyName);
-  print(myMonitor.model);
-  myMonitor.printScreenSize();
-
-  Monitor anikMonitor = Monitor();
-
-  anikMonitor.companyName = 'Samsung';
-  anikMonitor.model = 'A7';
-
-
-  print(anikMonitor.companyName);
-  print(anikMonitor.model);
-
-  Monitor rupMonitor = Monitor();
-
-  rupMonitor.companyName = 'Chinese';
-  rupMonitor.model = 'Unknown';
-
-  print(rupMonitor.companyName);
-  print(rupMonitor.model);
-  print(rupMonitor._internalHardwareNumber);
-}
