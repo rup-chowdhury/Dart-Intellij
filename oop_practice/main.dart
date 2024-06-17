7582951d04764a2655085f823752831dff18d8f3
Rup Chowdhury <rupbd1@gmail.com>
2024-06-17 10:09:30 +0600

    add main class into here

---
+++ oop_practice/main.dart
@@ -0,0 +1,25 @@
+void main(){
+  Monitor myMonitor = Monitor();
+
+  print(myMonitor.companyName);
+  print(myMonitor.model);
+  myMonitor.printScreenSize();
+
+  Monitor anikMonitor = Monitor();
+
+  anikMonitor.companyName = 'Samsung';
+  anikMonitor.model = 'A7';
+
+
+  print(anikMonitor.companyName);
+  print(anikMonitor.model);
+
+  Monitor rupMonitor = Monitor();
+
+  rupMonitor.companyName = 'Chinese';
+  rupMonitor.model = 'Unknown';
+
+  print(rupMonitor.companyName);
+  print(rupMonitor.model);
+  print(rupMonitor._internalHardwareNumber);
+}
