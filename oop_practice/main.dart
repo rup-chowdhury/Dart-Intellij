9829af9beda9d98ca1b9aaea43e71b35f7a88328
Rup Chowdhury <rupbd1@gmail.com>
2024-06-18 12:39:30 +0600

    Update main.dart

--- oop_practice/main.dart
+++ oop_practice/main.dart
@@ -2,26 +2,19 @@ import 'class_object.dart';
 
 void main(){
   Monitor myMonitor = Monitor( 'LG', 'K 150');
-  //
-  // print(myMonitor.companyName);
-  // print(myMonitor.model);
+
+  print(myMonitor.companyName);
+  print(myMonitor.model);
   myMonitor.printScreenSize();
 
   Monitor anikMonitor = Monitor('Samsung', 'A7');
 
-  // anikMonitor.companyName = 'Samsung';
-  // anikMonitor.model = 'A7';
-
-
-  // print(anikMonitor.companyName);
-  // print(anikMonitor.model);
+  print(anikMonitor.companyName);
+  print(anikMonitor.model);
 
   Monitor rupMonitor = Monitor('Chinese', 'Unknown');
 
-  // rupMonitor.companyName = 'Chinese';
-  // rupMonitor.model = 'Unknown';
-  //
-  // print(rupMonitor.companyName);
-  // print(rupMonitor.model);
+  print(rupMonitor.companyName);
+  print(rupMonitor.model);
   // print(rupMonitor._internalHardwareNumber);
 }
