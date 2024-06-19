61b7ea923f0ce47ee6f20f3755c13ab9dc1af7d6
Rup Chowdhury <rupbd1@gmail.com>
2024-06-19 17:48:42 +0600

    Update main.dart

--- oop_practice/main.dart
+++ oop_practice/main.dart
@@ -1,29 +1,41 @@
 import 'class_object.dart';
+import 'person.dart';
 
 void main(){
-  Monitor myMonitor = Monitor( 'LG', 'K 150');
+  // Monitor myMonitor = Monitor( 'LG', 'K 150');
+  //
+  // print(myMonitor.companyName);
+  // print(myMonitor.model);
+  // myMonitor.printScreenSize();
+  //
+  // Monitor anikMonitor = Monitor('Samsung', 'A7');
+  //
+  // print(anikMonitor.companyName);
+  // print(anikMonitor.model);
+  //
+  // Monitor rupMonitor = Monitor('Chinese', 'Unknown');
+  //
+  // print(rupMonitor.companyName);
+  // print(rupMonitor.model);
+  //
+  // rupMonitor.model = 'Known';
+  // print(rupMonitor.model);
+  // // print(rupMonitor._internalHardwareNumber);
+  //
+  // print(rupMonitor.deviceType);
+  //
+  // print(Monitor.sample);
+  //
+  // Monitor.getSampleData();
 
-  print(myMonitor.companyName);
-  print(myMonitor.model);
-  myMonitor.printScreenSize();
 
-  Monitor anikMonitor = Monitor('Samsung', 'A7');
+  Person rup = Person('Rup Chowdhury', 'Dhaka, Bangladesh', 25);
+  rup.printName();
 
-  print(anikMonitor.companyName);
-  print(anikMonitor.model);
 
-  Monitor rupMonitor = Monitor('Chinese', 'Unknown');
 
-  print(rupMonitor.companyName);
-  print(rupMonitor.model);
 
-  rupMonitor.model = 'Known';
-  print(rupMonitor.model);
-  // print(rupMonitor._internalHardwareNumber);
 
-  print(rupMonitor.deviceType);
-  
-  print(Monitor.sample);
 
-  Monitor.getSampleData();
+
 }
