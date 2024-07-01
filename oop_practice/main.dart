32cad292e5497478837d2e996fee089ee524cfe2
Rup Chowdhury <rupbd1@gmail.com>
2024-07-01 23:30:41 +0600

    Update main.dart

--- oop_practice/main.dart
+++ oop_practice/main.dart
@@ -39,5 +39,5 @@ void main(){
   two.printAddress();
   two.printAge();
 
-  Person three = Person(name: name, address: address, age: age)
+  Person three = Person(name: 'Tasin', address: 'Old Dhaka', age: )
 }
