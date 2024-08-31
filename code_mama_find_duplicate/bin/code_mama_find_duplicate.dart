d3c8d3793f96ab0f52d0b6a3edd4d1d6e2b6496a
Rup Chowdhury <rupbd1@gmail.com>
2024-09-01 01:16:41 +0600

    Update code_mama_find_duplicate.dart

--- code_mama_find_duplicate/bin/code_mama_find_duplicate.dart
+++ code_mama_find_duplicate/bin/code_mama_find_duplicate.dart
@@ -6,16 +6,20 @@ void main(){
 
   var data = array.split(" ");
 
+  num(data);
+}
+
+num (var data){
+  // print(data);
   for(int i = 0; i < data.length ; i++) {
     // print(data[i]);
     for(int j = 0 ; j < data.length ; j++){
       // print(data[j]);
 
-    if(i != j && j != i && data[i] == data[j] ){
-        print(data[j]);
-
+      if( i != j && j != i && data[i] == data[j] ){
+        print(data[i]);
+        return data[i];
       }
     }
-    break;
   }
 }
