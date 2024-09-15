4be8806aff9090a23820255d862cb61098486a0b
Rup Chowdhury <rupbd1@gmail.com>
2024-09-15 13:44:30 +0600

    new project

---
+++ problem_fourteen_one/bin/problem_fourteen_one.dart
@@ -0,0 +1,5 @@
+import 'package:problem_fourteen_one/problem_fourteen_one.dart' as problem_fourteen_one;
+
+void main(List<String> arguments) {
+  print('Hello world: ${problem_fourteen_one.calculate()}!');
+}
---
+++ problem_fourteen_one/test/problem_fourteen_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:problem_fourteen_one/problem_fourteen_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
