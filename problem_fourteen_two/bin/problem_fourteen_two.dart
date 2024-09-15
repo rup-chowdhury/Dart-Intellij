6da9cf14c380003eead15a645070312e89e3a260
Rup Chowdhury <rupbd1@gmail.com>
2024-09-15 15:02:37 +0600

    done

--- problem_fourteen_two/bin/problem_fourteen_two.dart
+++ problem_fourteen_two/bin/problem_fourteen_two.dart
@@ -1,13 +1,15 @@
 import 'dart:io';
 
 void main() {
-  // Write your dart code from here
+
   String input = stdin.readLineSync()!;
 
   for(int i = 0; i < input.length; i++){
-    if(i == 2 || i % 3 == 2){
-      stdout.write(input[i].toUpperCase());
-    }else{
+    if(i == 1 || i % 2 == 1 && i != input.length - 1){
+      stdout.write(input[i]);
+      stdout.write("x");
+    }
+    else{
       stdout.write(input[i]);
     }
   }
